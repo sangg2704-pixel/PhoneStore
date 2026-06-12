67d22a413935896c54e988d4b84335ad4e4d19d3
Admin <sangg2704@gmail.com>
2026-06-12 16:34:52 +0700

    Add project files.

---
+++ PhoneStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhoneStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
